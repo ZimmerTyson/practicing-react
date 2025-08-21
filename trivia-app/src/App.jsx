a5523aa3c35a90ba507c0c9e170b4a9f6977bf7b
Tyson Zimmer <zimmertyson@gmail.com>
2025-08-21 11:24:48 -0700

    Added the state functionality to shift between start, question, and the result states. Next  we need to incorporate the api into the mix

--- trivia-app/src/App.jsx
+++ trivia-app/src/App.jsx
@@ -2,15 +2,35 @@ import './App.css';
 import QuestionComponent from "./components/QuestionComponent";
 import StartComponent from "./components/StartComponent";
 import ResultsComponent from "./components/ResultsComponent";
+import {useState, useEffect} from "react";
+
+const ENDPOINT = "https://opentdb.com/api.php?amount=1&category=18&difficulty=easy&type=boolean";
 
 function App() {
+
+  const [gameState, setGameState] = useState(0);
+
+  async  function fetchNextQuestion() {
+    const res = await fetch(ENDPOINT);
+    const data = await res.json();
+    console.log("Question", data);
+  }
+
+  useEffect(()=>{
+    console.log("Use Effect")
+  }, [gameState]);
+
+  console.log("inside app component 1");
+
+  
+
 
   return (
     <div className="container">
       <h1>Trivia App</h1>
-      <StartComponent />
-      <QuestionComponent question="RAM stands for Random Access Memory."/>
-      <ResultsComponent result={12} />
+      {gameState == 0 && <StartComponent onClick={()=> setGameState(1)} />}
+      {gameState == 1 && (<QuestionComponent onAnswerSelect={ () => setGameState(2)} question="RAM stands for Random Access Memory."/>)}
+      {gameState == 2 && <ResultsComponent result={12} resetGame={()=> setGameState(1)} />}
     </div>
   );
 }
